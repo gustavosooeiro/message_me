a116938c979fe56efa86c49b04505fdbf90b74f2
Gustavo Soeiro Senise <gustavosooeiro@gmail.com>
2019-03-28 17:18:28 -0300

    Websockets working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'chatroom#index'
@@ -6,5 +7,7 @@ get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
 
+post 'message', to: 'messages#create'
 
+mount ActionCable.server, at: '/cable'
 end
